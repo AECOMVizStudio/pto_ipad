98ba75be4bcb5de4b83285c79fa5dde77db80ae3
Col-R <2colewestin@gmail.com>
2023-05-02 11:28:19 -0500

    removed placeholder backgrounds, added real text, began conditional rendering on component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import './App.css'
 import {Box, Grid, GridItem, Stack, Image, VStack, Flex, Text, Button} from '@chakra-ui/react'
 
-
+// images
+import logo from './assets/logo_aecom.png'
 
 // components
 import CardItem from './Components/CardItem'
 
 function App() {
-  const props = {
-    title: 'Card Title', 
-    text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sint ipsam esse.', 
-    imageUrl: 'http://via.placeholder.com/640x360'
-   
+  const props1 = [
+    { 
+    imageUrl: 'http://via.placeholder.com/640x360' 
+    },
+    {
+    title: 'Process Development', 
+    text: 'Life Cycle R&D Services from Concept Through Commercial Operation', 
+    imageUrl: 'http://via.placeholder.com/640x360' 
+    },
+    {
+    title: 'Laboratory and Analytical Services', 
+    imageUrl: 'http://via.placeholder.com/640x360' 
+    },
+];
+  const props2 = { 
+    imageUrl: 'http://via.placeholder.com/640x360' 
   };
 
 
   return (
     <>
     <Box
-      bgImage="url('https://img.freepik.com/free-photo/abstract-luxury-blur-dark-grey-black-gradient_1258-53740.jpg?w=1480&t=st=1682695106~exp=1682695706~hmac=7823da520517c1c01102fea84700c2f2d9d449f684beb864f9958a877d3cbf75')"
+      bgImage="url('https://images.unsplash.com/photo-1508791290064-c27cc1ef7a9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80')"
       backgroundSize="cover"
       backgroundPosition="center"
       height="100vh"
@@ -37,36 +49,44 @@ function App() {
         color='blackAlpha.700'
         fontWeight='bold'
       >
-        <GridItem pl='2' bg='orange.300' area={'header'}>
-          <Flex justify="space-between" align="center" p="4">
-            
-            <Text fontSize="2xl" fontWeight="bold">Process Technologies</Text>
-            <Image src="http://via.placeholder.com/200x100" alt="Logo" />
+        <GridItem pl='2' bg='transparent' area={'header'}>
+          <Flex justify="space-between" alignItems="center" p="4">
+            <Text fontSize="3xl" fontWeight="light"       
+              css={{
+              WebkitTextStroke: "1px black",
+              textStroke: "1px black",
+              }}>
+              Process <br></br>Technologies</Text>
+            <Image src={logo} alt="aecom logo" w="150px"/>
           </Flex>
         </GridItem>
-        <GridItem p='2' bg='blue.300' area={'sidebar'}>
+        <GridItem p='2' bg='transparent' area={'sidebar'}>
           <VStack align="stretch">
-            <Text fontSize="3xl" fontWeight="light" color="white">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium cum ullam animi quos debitis. Nemo, placeat.
+            <Text fontSize="3xl" fontWeight="light"       
+            css={{
+            WebkitTextStroke: "1px white",
+            textStroke: "1px white",
+            }}>
+              Delivering a better world by helping clients operate, maintain, and expand assets in a sustainable manner.
             </Text>
             <Button colorScheme='teal' variant='solid'>
               Learn More
             </Button>
-            <Button colorScheme='teal' variant='outline'>
+            <Button colorScheme='teal' bg="white" color="teal" variant='solid'>
               Contact Us
             </Button>
           </VStack>
         </GridItem>
-        <GridItem pl='2' bg='green.300' area={'main'}>
+        <GridItem pl='2' bg='transparent' area={'main'}>
             <Stack direction={'row'} justify='center' wrap='wrap' mt='2' p='2' spacing='25px'>
 
-                  <CardItem {...props}/>
-                  <CardItem {...props}/>
-                  <CardItem {...props}/>
+                  <CardItem {...props1[0]}/>
+                  <CardItem {...props1[1]}/>
+                  <CardItem {...props1[2]}/>
             </Stack>
             <Stack direction={'row'} justify='center' wrap='wrap' mt='2' p='2' spacing='25px'>
-                  <CardItem {...props}/>
-                  <CardItem {...props}/>
+                  <CardItem {...props2}/>
+                  <CardItem {...props2}/>
             </Stack>
           {/* <HStack>
             <CardItem />
--- src/Components/CardItem.tsx
+++ src/Components/CardItem.tsx
@@ -1,25 +1,33 @@
 import { Card, CardBody, Heading, Stack, Image, Text } from '@chakra-ui/react'
 
 interface CardItemProps {
-  title: string;
-  text: string;
+  title?: string | null;
+  text?: string | null;
   imageUrl: string;
 }
 
 const CardItem: React.FC<CardItemProps> = ({ title, text, imageUrl }) => {
+  // make the card dynamic based on the props it gets.
+  // if there is no Text, center the Title
   return (
-  <Card w='250px'>
+  <Card w='250px' bg="transparent" border="none" boxShadow = "none">
     <CardBody>
       <Image
         src={imageUrl}
         alt='brochure'
         borderRadius='lg'
       />
-      <Stack spacing='3' opacity="0.8">
+      <Stack spacing='3' bg="white" p="2" borderRadius='10'>
         <Heading size='md'>{title}</Heading>
         <Text>
           {text}
         </Text>
+        {text && 
+          <Text>
+            Download Brochure
+          </Text>
+          }
+
       </Stack>
     </CardBody>
   </Card>
