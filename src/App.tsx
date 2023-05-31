bd1afdc95343d5436f8f8ba9f6f138ccb34af1fa
Col-R <2colewestin@gmail.com>
2023-05-31 11:51:00 -0500

    numerous changes, images, alignment, QR code add, and other stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import {Box, Grid, GridItem, Stack, Image, VStack, Flex, Text, Button, Link} from '@chakra-ui/react'
+import {useState} from 'react'
 
 // images
 import logo from './assets/logo_aecom.png'
@@ -8,50 +9,41 @@ import logo from './assets/logo_aecom.png'
 import CardItem from './Components/CardItem'
 
 function App() {
-  const contact = 'scott.d.miller@aecom.com'
+  const [showQR, setShowQR] = useState(false);
+  const handleToggle = () => {setShowQR(!showQR)}
+
   const props1 = [
     { 
-      title: 'Laboratory and Analytical Services', 
-      imageUrl: './lab_service_sm.webp',
-      alt:'Laboratory and Analytical Services',
-      link: 'https://www.aecomprocesstechnologies.com/wp-content/uploads/2016/08/AECOM-Process-Technologies-Laboratory-and-Analytical-Services.pdf'
-    },
-    {
-    title: 'Process Development', 
-    text: 'Life Cycle R&D Services from Concept Through Commercial Operation', 
-    imageUrl: './process_dev_sm.webp' ,
-    alt:'process development life cycle R&D',
-    link:'https://www.aecom.com/wp-content/uploads/2019/07/13_Process-Development-Brochure-Aug-2018.pdf'
-    },
-    {
-    title: 'EPC Services', 
-    imageUrl: './EPC_sm.webp',
-    alt:'EPC Services, process technologies',
-    link:'https://aecomprocesstechnologies.com/wp-content/uploads/2018/01/PTO-EPC-Services_singles.pdf'
-    },
+      imageUrl: './small_carbon.png',
+      alt:'Carbon Capture for Utilization and storage',
+      link: 'https://indd.adobe.com/view/71a3aa91-2100-4703-827c-15a988fed0be'
+      },
+      {
+      imageUrl: './process_dev_sm.webp' ,
+      alt:'process development life cycle R&D',
+      link:'https://www.aecom.com/wp-content/uploads/2019/07/13_Process-Development-Brochure-Aug-2018.pdf'
+      },
+      { 
+        imageUrl: './small_future.png',
+        alt:'low carbon future',
+        link:'https://indd.adobe.com/view/bf8b65b5-b3ca-46bb-bf90-92e02963afc3'
+      },
+      { 
+        imageUrl: './lab_service_sm.webp',
+        alt:'Laboratory and Analytical Services',
+        link: 'https://www.aecomprocesstechnologies.com/wp-content/uploads/2016/08/AECOM-Process-Technologies-Laboratory-and-Analytical-Services.pdf'
+      },
+      
+      
 ];
-  const props2 = [
-    { 
-    imageUrl: './small_carbon.png',
-    alt:'Carbon Capture for Utilization and storage',
-    link: 'https://indd.adobe.com/view/71a3aa91-2100-4703-827c-15a988fed0be'
-    },
-    { 
-    imageUrl: './small_future.png',
-    alt:'low carbon future',
-    link:'https://indd.adobe.com/view/bf8b65b5-b3ca-46bb-bf90-92e02963afc3'
-    },
-
-  ];
-
 
   return (
     <>
     <Box
-      bgImage="url('https://images.unsplash.com/photo-1500382017468-9049fed747ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80')"
+      bgImage="url('https://images.unsplash.com/photo-1592722182837-52273392552d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1172&q=80')"
       backgroundSize="cover"
       backgroundPosition="center"
-      height="100vh"
+      height="100%"
       position="fixed"
       width="100%"
       zIndex="-1"
@@ -91,42 +83,45 @@ function App() {
           mx={1}
           >
           <VStack align="stretch" ml='1'>
+          <Box>
+
+          {showQR ?
+            <img src="./qr.png" alt="qr code" style={{ display: "block", width: "200px",  }} />
+            :
             <Text fontSize="3xl" fontWeight="light"        
             css={{
-            WebkitTextStroke: "1px white",
-            textStroke: "1px white",
+              WebkitTextStroke: "1px white",
+              textStroke: "1px white",
             }}>
               Delivering a better world by helping clients operate, maintain, and expand assets in a sustainable manner.
             </Text>
+          }   
             <Link href = 'https://aecom.com/services/process-technologies/' target='_blank' width='100%'>
-              <Button width="100%" colorScheme='teal' variant='solid'>
+              <Button width="100%" colorScheme='teal' variant='solid' my={2}>
                 Learn More
               </Button>
             </Link>
-            <Link href = {`mailto:${contact}`} width='100%'>
-              <Button width="100%" colorScheme='white' bg="white" color="teal" variant='solid'>
-                Contact Us
+
+            {showQR ? 
+              <Button onClick = {handleToggle} width="100%" colorScheme='white' bg="white" color="teal" variant='solid'>
+                Our Mission
               </Button>
-            </Link>
+            :<Button onClick = {handleToggle} width="100%" colorScheme='white' bg="white" color="teal" variant='solid'>
+              Contact Us
+            </Button>}
+
+          </Box>
           </VStack>
         </GridItem>
-        <GridItem bg='transparent' area={'main'}>
-            
-            <Stack direction={'row'} justify='center' wrap='wrap'>
+        <GridItem bg='transparent' area={'main'} m={0}>   
+  
+            <Stack direction={'row'} justify='center' wrap='wrap' m={0}>
                   <CardItem {...props1[0]}/>
                   <CardItem {...props1[1]}/>
                   <CardItem {...props1[2]}/>
+                  <CardItem {...props1[3]}/>
+
             </Stack>
-          <div style = {{margin: "8em 0"}}>
-            <Stack direction={'row'} justify='center' wrap='wrap' mt='2' spacing={12}>
-                  <CardItem {...props2[0]}/>
-                  <CardItem {...props2[1]}/>
-            </Stack>
-          </div>
-          {/* <HStack>
-            <CardItem />
-            <CardItem />
-          </HStack> */}
         </GridItem>
       </Grid>
 
